b73f6b1b2d339aa1956969cf4731f39c887d8bd4
Seongho Lee <tjdgh2626@gmail.com>
2023-12-12 20:28:41 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,25 +5,20 @@
 #include <avr/iom128.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include <stdlib.h>
 
 #include "serial.h"
 #include "mpu6050.h"
 #include "motor.h"
+#include "angle.h"
 
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-#define TRUE    1
-#define FALSE   0
-
-volatile float accel_y;
+volatile float error;
 volatile float prev_error;
 
 SIGNAL(MOTOR_CONTROL_IRQ) {
     static const float dt = 0.0005f; // interrupt for every 500us
-    static const float kp = 0.03f; // proportional gain
-    static const float kd = 0.02f; // derivative gain
+    static const float kp = 0.003f; // proportional gain
+    static const float kd = 0.0f; // derivative gain
 
-    float error = accel_y;
     float error_diff = (error - prev_error) * dt;
 
     float duty_ratio = kp * error + kd * error_diff;
@@ -38,19 +33,18 @@ SIGNAL(MOTOR_CONTROL_IRQ) {
 
 int main(void)
 {
+    sei();
+
     serial_init();
     mpu6050_init();
     motor_init();
-    sei();
+    angle_init();
 
-    motor_control_enabled(TRUE);
-    for (;;) 
-    {
-        // accel_x = mpu6050_get_accel_x();
-        accel_y = mpu6050_get_accel_y();
-        // accel_z = mpu6050_get_accel_z();
+    angle_enabled(1);
+    motor_control_enabled(1);
 
-        // serial_printf("accel_x: %+2.4f, accel_y: %+2.4f, accel_z: %+2.4f\r\n", accel_x, accel_y, accel_z);
-        serial_printf("accel_y: %+2.4f speed_l: %3d speed_r: %3d\r\n", accel_y, OCR1A, OCR1B);
+    for (;;) {
+        error = angle_retrieve();
+        serial_printf("pitch: %+2.4f, OCR1A: %3d \r\n", angle_retrieve(), OCR1A);
     }
 }
